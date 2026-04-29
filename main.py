4522a445d7f0dba1a062071e81967b8031314afa
Polyploy05 <jakada005@gmail.com>
2026-04-29 12:50:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@
 Name: Jacob Miranda & Daniel Puerto
 Date: 4/29/26
 Group: 17
-Description: 
+Description:
 '''
 
-
+from task import Task
 from tasklist import Tasklist 
 import check_input
 
 def main_menu():
-    #Creates the main menu for the user and checks valid inputs
     print("\n-Tasklist")
     print("1. Display Current Task")
     print("2. Display all task")
@@ -24,28 +23,42 @@ def main_menu():
         return choice
 
 def get_date():
-    # Asks the user to enter the month, day and year, and checks validity. 
     print("Enter due date:")
     month = check_input.get_int_range("Enter month: ", 1, 12)
-    day = check_input.get_int_range("Enter day: ", 0, 31)
-    year = check_input.get_int_range("Enter year: ", 2000, 2010)
+    if month < 10:
+        month = "0" + str(month)
+    else:
+        month = str(month)
+    day = check_input.get_int_range("Enter day: ", 1, 31)
+    if day < 10:
+        day = "0" + str(day)
+    else:
+        day = str(day)
+    year = str(check_input.get_int_range("Enter year: ", 2000, 2100))
 
     return month + "/" + day + "/" + year
 
 
 def get_time():
-    # Prompts the user to enter the hour (military time) and minute. And returns the date in HH:MM
     print("Enter time:")
 
     hour = check_input.get_int_range("Enter hour: ", 1, 12)
+    if hour < 10:
+        hour = "0" + str(hour)
+    else:
+        hour = str(hour)
     minute = check_input.get_int_range("Enter minute: ", 0, 59)
+    if minute < 10:
+        minute = "0" + str(minute)
+    else:
+        minute = str(minute)
 
     return hour + ":" + minute
 
 def main():
-    #D
+
     filename = "tasklist.txt"
-    tasklist = Tasklist(tasklist.txt)
+    tasklist = Tasklist(filename)
 
     while True:
         print(f"\nTasks to complete: {len(tasklist)}")
@@ -71,15 +84,15 @@ def main():
         elif choice == 3:
             task = tasklist.mark_complete()
             if task:
-                print("Completed:", task)
+                print("Completed: ", task)
                 new_task = tasklist.get_current_task()
                 if new_task:
-                    print("Next task:", new_task)
+                    print("Next task: ", new_task)
                 else:
                     print("All tasks are completed")
 
         elif choice == 4:
-            desc = input("Enter a task:").strip()
+            desc = input("Enter a task: ").strip()
             date = get_date()
             time = get_time()
             tasklist.add_task(desc,date,time)
@@ -104,3 +117,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
