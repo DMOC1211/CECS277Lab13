f0a43b215c8e17d6462ae4001773b999bcdebb6f
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-29 12:09:30 -0700

    Update task.py

--- task.py
+++ task.py
@@ -2,4 +2,17 @@
 '''
 
 
+class Task:
+    def _init_(self, desc, date, time):
+        self.description = desc
+        self.date = date #MM/DD/YYYY
+        self.time = time #HH:MM
 
+    def __str__(self):
+        return f"{self.description} (Due: {self.date} at {self.time})"
+    
+    def __repr__(self):
+        return f"{self.description}, {self.date}, {self.time}"
+    
+    def __lt__(self,other):
+        m1, d1, y1 = map(int, self.date.split)
