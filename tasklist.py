a94dd7aaff345b5d6a206bfb2aa6d0e4df4c0852
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-29 12:56:51 -0700

    Update tasklist.py

--- tasklist.py
+++ tasklist.py
@@ -10,6 +10,7 @@ from task import Task
 
 class Tasklist:
     def __init__(self,tasklist):
+        # Reads the list of tasks from the file and stores them by opening the file
         self.tasklist = []
         self.filename = Task
         self.list = tasklist
@@ -27,33 +28,41 @@ class Tasklist:
             pass
     
     def add_task(self,desc,date,time):
+        #Creates a new task using the parameters, adds it to the tasklist and sorts the list
         new_task = Task(desc,date,time)
         self.tasklist.append(new_task)
         self.tasklist.sort()
 
     def get_current_task(self):
+        #Returns the task that is at the beginning of the list
         if len(self.tasklist) == 0:
             return None
         return self.tasklist[0]
     
     def mark_complete(self):
+        #Removes and returns the current task from the list
         if len(self.tasklist) == 0:
             return None
         return self.tasklist.pop(0)
     
     def save_file(self):
+        # Writes hte content of the tasklist back to the file using the repr method
         with open(self.list, 'w') as file:
             for task in self.tasklist:
                 file.write(repr(task)+ '\n')
 
     def __len__(self):
+        # Returns the number of items in the tasklist
         return len(self.tasklist)
     
     def __iter__(self):
+        # Initializes the iterator attribute 'n' and returns itself
         self.n = 0 
         return self
     
     def __next__(self):
+        # Iterate the iterator at each position.  Raises a StopIteration when the iterator reaches 
+        # the end of the tasklist, if not, returns the Task object at the iterator's current positon
         if self.n >= len(self.tasklist):
             raise StopIteration
         task = self.tasklist[self.n]
