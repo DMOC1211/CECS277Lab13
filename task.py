9044477d4455b5e576d43814dbfca0c0bf3103c3
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-29 12:33:15 -0700

    Update task.py

--- task.py
+++ task.py
@@ -15,12 +15,16 @@ class Task:
         self.time = time #HH:MM
 
     def __str__(self):
+        #Displays the task's information to the user
         return f"{self.description} (Due: {self.date} at {self.time})"
     
     def __repr__(self):
+        #Writes the task to the file
         return f"{self.description}, {self.date}, {self.time}"
     
     def __lt__(self,other):
+        #Compares by year, month, day, hour, minute and then description 
+        #by alphabetical order. (In the order it was written)
         m1, d1, y1 = map(int, self.date.split)
         h1, min1 = map(int, self.time.split(':'))
 
